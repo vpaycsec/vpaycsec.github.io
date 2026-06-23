2a158d9936f961bd42ff12d39ebd9bf8f1d544df
Vedant Prasad <116428495+vpaycsec@users.noreply.github.com>
2026-06-24 01:50:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.onload = function () {
-console.log("Script loaded");
 
-```
-setTimeout(function () {
-    document.getElementById("gateway").style.display = "none";
-    document.getElementById("portfolio").style.display = "block";
-}, 3000);
-```
+    console.log("Script loaded");
+
+    setTimeout(function () {
+
+        document.getElementById("gateway").style.display = "none";
+
+        document.getElementById("portfolio").style.display = "block";
+
+    }, 3000);
 
 };
