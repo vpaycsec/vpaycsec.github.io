e300582edaa9fe9b971548d08239c79582e43a8b
Vedant Prasad <116428495+vpaycsec@users.noreply.github.com>
2026-06-24 01:48:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 window.onload = function () {
+console.log("Script loaded");
 
 ```
-const sessionId =
-    "CV-" +
-    Math.random().toString(36).substring(2, 8).toUpperCase();
-
-document.getElementById("session").innerText = sessionId;
-
-const browser = navigator.userAgent;
-const platform = navigator.platform;
-const language = navigator.language;
-const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const screenSize = screen.width + " x " + screen.height;
-
-document.getElementById("browser").innerText =
-    browser.includes("Chrome") ? "Chrome" :
-    browser.includes("Firefox") ? "Firefox" :
-    browser.includes("Safari") ? "Safari" :
-    "Unknown";
-
-document.getElementById("platform").innerText = platform;
-document.getElementById("language").innerText = language;
-document.getElementById("timezone").innerText = timezone;
-document.getElementById("screen").innerText = screenSize;
-
 setTimeout(function () {
-
     document.getElementById("gateway").style.display = "none";
     document.getElementById("portfolio").style.display = "block";
-
-}, 3500);
+}, 3000);
 ```
 
 };
